84a44f8e9a5e494cbf66c1e18a4f3b4ebc7a4a3c
Yashodhan Sharma <81512294+iYashodhan@users.noreply.github.com>
2021-09-15 23:53:23 +0530

    re-written with pure functions
    OOP is not IT!!!!

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -1,65 +1,61 @@
-import random
 import running
+import random
 
 
-class Sudoku:
+def printBoard(board):
 
-    def __init__(self):
-        self.board = random.choice(running.boards)
+    for i in range(len(board)):
+        if i % 3 == 0:
+            print('- - - - - - - - - - - - -')
 
-    def printBoard(self):
+        for j in range(len(board[i])):
+            if j % 3 == 0:
+                print('| ' + str(board[i][j]) + ' ', end='')
 
-        for i in range(len(self.board)):
-            if i % 3 == 0:
-                print('- - - - - - - - - - - - -')
+            elif j == 8:
+                print(str(board[i][j]) + ' |')
 
-            for j in range(len(self.board[i])):
-                if j % 3 == 0:
-                    print('| ' + str(self.board[i][j]) + ' ', end='')
+            else:
+                print(str(board[i][j]) + ' ', end='')
 
-                elif j == 8:
-                    print(str(self.board[i][j]) + ' |')
+        if i == 8:
+            print('- - - - - - - - - - - - -')
 
-                else:
-                    print(str(self.board[i][j]) + ' ', end='')
 
-            if i == 8:
-                print('- - - - - - - - - - - - -')
+def ifEmpty(board):  # an empty is needed to solve and apply algorithims
 
-    def ifEmpty(self):  # returns the position of an empty cell
-
-        for i in range(len(self.board)):
-            for j in range(len(self.board)):
-                if self.board[i][j] == 0:
+        for i in range(len(board)):
+            for j in range(len(board)):
+                if board[i][j] == 0:
                     return i, j
 
         return None
 
-    def validNumReplacement(self, num, position_cell):
 
-        # Check if num exists in Row
-        for i in range(len(self.board[position_cell[0]])):
-            if self.board[position_cell[0]][i] == num and position_cell[1] != i:
+def validNumReplacement(board, num, position_cell):
+
+        # Check if num exists in Row, sudoku rules
+        for i in range(len(board[position_cell[0]])):
+            if board[position_cell[0]][i] == num and position_cell[1] != i:
                 return False
 
-        # Check if num exists in Column
-        for j in range(len(self.board[position_cell[1]])):
-            if self.board[j][position_cell[1]] == num and position_cell[0] != j:
+        # Check if num exists in Column, sudoku rules
+        for j in range(len(board[position_cell[1]])):
+            if board[j][position_cell[1]] == num and position_cell[0] != j:
                 return False
 
-        # Checking num in the box
+        # Checking num in the box, sudoku rules
         box_row = position_cell[0] // 3
         box_col = position_cell[1] // 3
 
         for i in range(box_row*3, box_row*3 + 3):
             for j in range(box_col*3, box_col*3 + 3):
-                if self.board[i][j] == num and (i, j) != position_cell:
+                if board[i][j] == num and (i, j) != position_cell:
                     return False
+                    
 
-        return True
-
-    def solveSudoku(self):
-        find = self.ifEmpty()
+def solveSudoku(board):
+        find = ifEmpty(board)
 
         if not find:
             return True
@@ -67,21 +63,21 @@ class Sudoku:
             position = find
 
         for tryNum in range(1, 10):
-            if self.validNumReplacement(tryNum, position):
-                self.board[position[0]][position[1]] = tryNum
+            if validNumReplacement(board, tryNum, position):
+                board[position[0]][position[1]] = tryNum
 
-                if self.solveSudoku():
+                if solveSudoku():
                     return True
 
-                self.board[position[0]][position[1]] = 0
+                board[position[0]][position[1]] = 0
 
         return False
 
 
 if __name__ == '__main__':
 
-    game = Sudoku()
-    game.printBoard()
-    game.solveSudoku()
+    gameBoard = random.choice(running.boards)
+    printBoard(gameBoard)
+    solveSudoku(gameBoard)
     print("<<<<<<<<--Board-solved-->>>>>>")
-    game.printBoard()
+    printBoard(gameBoard)
